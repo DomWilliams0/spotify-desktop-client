22777ed843841d314cd33bfffecc774d5c1b5f1e
Dom Williams <me@domwillia.ms>
2017-06-21 00:41:34 +0200

    Add temporary loading of creds from env

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,29 @@ mod error;
 use spotify::Spotify;
 
 fn main() {
-    let mut spot = Spotify::new();
+    // temporarily read creds from environment
+    const CRED_ENV: &str = "SPOTIFY_CREDS";
+    let (user, password) = match std::env::var(CRED_ENV) {
+        Err(e) => {
+            println!("No creds provided in env var {} ({})", CRED_ENV, e);
+            std::process::exit(1);
+        }
+        Ok(ref creds) => {
+            match creds.find(":") {
+                None => {
+                    println!("Creds must be in format user:password");
+                    std::process::exit(2);
+                }
+                Some(index) => {
+                    let user = (&creds[..index]).to_owned();
+                    let pass = (&creds[index + 1..]).to_owned();
+                    (user, pass)
+                }
+            }
+        }
+    };
 
-    spot.authenticate("dummy", "dummy");
+    let mut spot = Spotify::new();
+    spot.authenticate(&user, &password);
 
 }
